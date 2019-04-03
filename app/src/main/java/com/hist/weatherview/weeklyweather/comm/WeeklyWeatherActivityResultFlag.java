3c48f83a133cc3b6102406edc4d54b12856c6677
jwjang <summsu84@gmail.com>
2019-04-03 17:30:05 +0900

    20190403 - JJW 1. REQUEST CODE 수정

--- app/src/main/java/com/hist/weatherview/weeklyweather/comm/WeeklyWeatherActivityResultFlag.java
+++ app/src/main/java/com/hist/weatherview/weeklyweather/comm/WeeklyWeatherActivityResultFlag.java
@@ -3,8 +3,8 @@ package com.hist.weatherview.weeklyweather.comm;
 public class WeeklyWeatherActivityResultFlag {
 
     // 주간 날씨 요청
-    public static final int WEEKLY_WEATHER_FAVORITE_AREA_REQUEST = 100001;
-    public static final int WEEKLY_WEATHER_SEARCH_AREA_REQUEST = 100002;
+    public static final int WEEKLY_WEATHER_FAVORITE_AREA_REQUEST = 20000;
+    public static final int WEEKLY_WEATHER_SEARCH_AREA_REQUEST = 20001;
 
     // 요청 결과 코드
     public static final int RESULT_OK = -1;
